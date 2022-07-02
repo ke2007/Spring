ec251fe195ff3af16adf1236b155d37ffb94e0ea
dlawn <dlawnsrjs@gmail.com>
2022-07-02 16:11:17 +0900

    07.02

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,12 +3,50 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
-    @GetMapping
+    @GetMapping("hello")
     public String hello(Model model) {
-        model.addAttribute("data", "Spring");
+        model.addAttribute("data", "hello");
         return "hello";
+    }
+
+    //
+// MVC방식
+    @GetMapping("hello-MVC")
+
+    public String helloMVC(@RequestParam(name = "name", required = true) String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    //    API방식
+    @GetMapping("hello-string")
+    @ResponseBody //html의 <body>가 아니고 http통신 프로토콜에서 body 부분에 데이터를 직접 넣겠다 .
+    public String helloString(@RequestParam("name") String name) {
+        return "hello" + name;
+    }
+
+    //    데이터를 내놔 API예제
+    @GetMapping("hello-API")
+    @ResponseBody //Json으로 반환하는게 default값 필요시에 XML로도 세팅가능하다(레거시프로젝트경우)
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name); //파라미터로 넘어온 name 전달
+        return hello; //hello라는 객체를 넘김=> Spring은 HttpMessageConverter가 객체를 웹브라우저든 서버든 요청한곳에 Json 방식 데이터를 만들어서 전달, MappingJackson2HttpMessageConverter가 객체를 처리
+    }
+    static class Hello{
+        private String name; // 1. private이기때문에 외부에선 사용 못하니까 자바bean규약
+
+        public String getName() {  // 2.외부에서 사용할때 메서드를 통해 프로퍼티방식으로 name을 전달
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
