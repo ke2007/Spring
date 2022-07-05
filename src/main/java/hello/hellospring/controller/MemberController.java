a7665aa4024ebcb37e9d2c32ca2c4e1a87b74575
dlawn <dlawnsrjs@gmail.com>
2022-07-05 16:47:49 +0900

    07.05 createMemberForm에서 넘어온 데이터를 MemberController POST방식으로 등록하고 memberForm 에서 getter setter방식으로 name을 꺼내오고 해당 name을 멤버에서 getter setter방식으로 member의 이름을 저장하고 memberservice의 join메서드 안에있는 memberRepository의 save메서드로 멤버를 저장

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,8 +1,14 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 
 @Controller
@@ -12,6 +18,28 @@ public class MemberController {
     @Autowired //멤버컨트롤러를 스프링컨테이너가 뜰때 생성하는데 그때 생성자를 호출한다, Autowired를 사용하면 자동으로 멤버서비스에 가져다가 연결시켜줌
     public MemberController(MemberService memberService) {  //생성자를 통해 memberService를 MemberController에 주입
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")  //데이터를 form같은데 넣어서 등록할떄 POST 사용 get은 대부분 조회할떄 사용
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
 
